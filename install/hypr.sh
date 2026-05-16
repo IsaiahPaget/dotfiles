6c0595d6a8ea85264ca5eb2d40ae7ac1313f2ca9
Isaiah Paget <isaiahpaget@gmail.com>
2026-05-15 20:29:27 -0700

    hyprsunset

--- install/hypr.sh
+++ install/hypr.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 sudo pacman -S hyprpaper --noconfirm --needed
+sudo pacman -S hyprsunset --noconfirm --needed
 sudo pacman -S hyprpolkitagent --noconfirm --needed
 sudo pacman -S hyprlock --noconfirm --needed
 
